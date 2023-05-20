9aa20d4a87a0c8bb8efcbc001718649b3b39b5ad
Mulamu <84395540+DevMulamu@users.noreply.github.com>
2023-05-21 00:38:30 +0200

    Added design to dashboard and added the Rotodo font family

--- app/src/main/java/com/example/dreamydo/MainActivity.kt
+++ app/src/main/java/com/example/dreamydo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.dreamydo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dreamydo.ui.Dashboard
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, Dashboard::class.java)
+        startActivity(intent)
     }
 }
