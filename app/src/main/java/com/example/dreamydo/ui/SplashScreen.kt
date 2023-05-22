e93b13156e5f5f3cce40cbbe7177a4f517c07096
Mulamu <vortexmammba@gmail.com>
2023-05-22 16:58:36 +0200

    Updated the splash screen to react to light/dark mode

--- app/src/main/java/com/example/dreamydo/ui/SplashScreen.kt
+++ app/src/main/java/com/example/dreamydo/ui/SplashScreen.kt
@@ -1,21 +1,21 @@
-package com.example.dreamydo
+package com.example.dreamydo.ui
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.dreamydo.ui.Dashboard
+import com.example.dreamydo.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     private val scope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
 
         startApp()
     }
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private fun startApp() {
         scope.launch {
             delay(3000)
-            val intent = Intent(this@MainActivity, Dashboard::class.java)
+            val intent = Intent(this@SplashScreen, Dashboard::class.java)
             startActivity(intent)
             finish()
         }
